0ffe19767fcc9069a3fb4beded065807b19a2312
Kauã Pereira <zkauacodes@hotmail.com>
2025-04-11 08:50:19 -0300

    test(integration-test-config): added the build properties bean

--- infrastructure/src/test/java/com/kaua/events/platform/IntegrationTestConfig.java
+++ infrastructure/src/test/java/com/kaua/events/platform/IntegrationTestConfig.java
@@ -1,25 +1,34 @@
 package com.kaua.events.platform;
 
+import com.kaua.events.platform.domain.utils.InstantUtils;
 import com.kaua.events.platform.infrastructure.services.rsakey.RsaKeyProvider;
-import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.info.BuildProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 
 import java.security.KeyPair;
 import java.security.KeyPairGenerator;
 import java.security.interfaces.RSAPrivateKey;
 import java.security.interfaces.RSAPublicKey;
 import java.util.Map;
+import java.util.Properties;
 import java.util.concurrent.ConcurrentHashMap;
 
 @Configuration(proxyBeanMethods = false)
 public class IntegrationTestConfig {
 
-//    @Profile("test")
     @Bean
     public RsaKeyProvider rsaKeyProviderTest() {
         return new InMemoryRsaKeyProvider();
+    }
+
+    @Bean
+    public BuildProperties buildProperties() {
+        Properties properties = new Properties();
+        properties.setProperty("name", "events-platform");
+        properties.setProperty("version", "0.0.1");
+        properties.setProperty("time", InstantUtils.now().toString());
+        return new BuildProperties(properties);
     }
 
     public static class InMemoryRsaKeyProvider implements RsaKeyProvider {
