5063a2fc0169a01983f595893cde3b2cb438b19d
Kauã Pereira <zkauacodes@hotmail.com>
2025-04-11 08:46:10 -0300

    feat(users-table-migrastion): added the create users table migrastion

---
+++ infrastructure/src/main/resources/db/migration/V1__create-users-table.sql
@@ -0,0 +1,11 @@
+CREATE TABLE users (
+    id VARCHAR(26) NOT NULL PRIMARY KEY,
+    first_name VARCHAR(110) NOT NULL,
+    last_name VARCHAR(110) NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    role VARCHAR(50) NOT NULL,
+    created_at TIMESTAMP WITH TIME ZONE NOT NULL,
+    updated_at TIMESTAMP WITH TIME ZONE NOT NULL,
+    version BIGINT NOT NULL
+);
