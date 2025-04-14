7c1fe90ca19d9146e7ad261c173773001665b648
Kauã Pereira <zkauacodes@hotmail.com>
2025-04-14 15:39:27 -0300

    refactor(oauth-authenticate-filter): refactor to use handler exception resolver

--- infrastructure/src/main/java/com/kaua/events/platform/infrastructure/oauth/OAuthAuthenticateFilter.java
+++ infrastructure/src/main/java/com/kaua/events/platform/infrastructure/oauth/OAuthAuthenticateFilter.java
@@ -1,5 +1,6 @@
 package com.kaua.events.platform.infrastructure.oauth;
 
+import com.kaua.events.platform.domain.exceptions.DomainException;
 import com.kaua.events.platform.infrastructure.configurations.properties.OAuthClients;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
@@ -7,15 +8,18 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.HttpHeaders;
 import org.springframework.lang.NonNull;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
 import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.web.servlet.HandlerExceptionResolver;
 
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+import java.util.Base64;
 import java.util.Objects;
 import java.util.stream.Collectors;
 
@@ -24,11 +28,14 @@ public class OAuthAuthenticateFilter extends OncePerRequestFilter {
     private static final Logger log = LoggerFactory.getLogger(OAuthAuthenticateFilter.class);
 
     private final OAuthClients oAuthClients;
+    private final HandlerExceptionResolver resolver;
 
     public OAuthAuthenticateFilter(
-            final OAuthClients oAuthClients
+            final OAuthClients oAuthClients,
+            final @Qualifier("handlerExceptionResolver") HandlerExceptionResolver resolver
     ) {
         this.oAuthClients = Objects.requireNonNull(oAuthClients);
+        this.resolver = Objects.requireNonNull(resolver);
     }
 
     @Override
@@ -44,7 +51,7 @@ public class OAuthAuthenticateFilter extends OncePerRequestFilter {
             String aBase64Credentials = aAuthHeader.substring("Basic ".length()).trim();
 
             try {
-                byte[] aDecoded = java.util.Base64.getDecoder().decode(aBase64Credentials);
+                byte[] aDecoded = Base64.getDecoder().decode(aBase64Credentials);
                 String aCredentials = new String(aDecoded, StandardCharsets.UTF_8);
 
                 String[] aParts = aCredentials.split(":", 2);
@@ -54,25 +61,23 @@ public class OAuthAuthenticateFilter extends OncePerRequestFilter {
 
                     log.debug("OAuthAuthenticateFilter [clientId:{}] authentication in progress", aClientId);
 
-                    var aOAuthClient = this.oAuthClients.getClients().get(aClientId);
+                    final var aOAuthClient = this.oAuthClients.getClient(aClientId);
 
-                    if (aOAuthClient == null || !aClientId.equalsIgnoreCase(aOAuthClient.clientId())) {
+                    if (aOAuthClient.isEmpty()) {
                         log.debug("OAuthAuthenticateFilter [clientId:{}] not found", aClientId);
-                        response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Invalid client credentials");
-                        return;
+                        throw DomainException.with("Invalid client credentials"); // TODO trocar a exception
                     }
 
-                    if (!aClientSecret.equals(aOAuthClient.clientSecret())) {
+                    if (!aClientSecret.equals(aOAuthClient.get().clientSecret())) {
                         log.debug("OAuthAuthenticateFilter [clientId:{}] invalid secret", aClientId);
-                        response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Invalid client credentials");
-                        return;
+                        throw DomainException.with("Invalid client credentials"); // TODO trocar a exception
                     }
 
                     final var authentication = new OAuth2ClientAuthenticationToken(
                             aClientId,
                             ClientAuthenticationMethod.CLIENT_SECRET_BASIC,
                             aClientSecret,
-                            aOAuthClient.authorities().stream()
+                            aOAuthClient.get().authorities().stream()
                                     .map(SimpleGrantedAuthority::new)
                                     .collect(Collectors.toSet())
                     );
@@ -83,12 +88,11 @@ public class OAuthAuthenticateFilter extends OncePerRequestFilter {
                     log.debug("OAuthAuthenticateFilter [clientId:{}] authenticated", aClientId);
                 }
             } catch (Exception ex) {
-                log.error("OAuthAuthenticateFilter error decoding credentials", ex);
-                response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Invalid client credentials");
+                log.error("OAuthAuthenticateFilter on authentication"); // TODO melhorar o log
+                resolver.resolveException(request, response, null, ex);
                 return;
             }
         }
-
         filterChain.doFilter(request, response);
     }
 }
