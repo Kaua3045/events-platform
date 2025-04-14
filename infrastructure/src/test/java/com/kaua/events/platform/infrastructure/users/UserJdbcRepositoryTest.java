2c588783efdbb41e7e09f324168a10800ebdbe3a
Kauã Pereira <zkauacodes@hotmail.com>
2025-04-14 15:45:56 -0300

    test(user-repository): added the user repository interface implementation jdbc userOfEmail and userOfId tests

--- infrastructure/src/test/java/com/kaua/events/platform/infrastructure/users/UserJdbcRepositoryTest.java
+++ infrastructure/src/test/java/com/kaua/events/platform/infrastructure/users/UserJdbcRepositoryTest.java
@@ -1,9 +1,11 @@
 package com.kaua.events.platform.infrastructure.users;
 
 import com.kaua.events.platform.AbstractRepositoryTest;
+import com.kaua.events.platform.domain.exceptions.NotFoundException;
 import com.kaua.events.platform.domain.users.*;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.test.context.jdbc.Sql;
 
 class UserJdbcRepositoryTest extends AbstractRepositoryTest {
 
@@ -75,5 +77,99 @@ class UserJdbcRepositoryTest extends AbstractRepositoryTest {
         final var aActualResponse = this.userRepository().existsByEmail(aEmail);
 
         Assertions.assertTrue(aActualResponse);
+    }
+
+    @Test
+    void givenAValidEmail_whenCallUserOfEmail_thenReturnUser() {
+        Assertions.assertEquals(0, countUsers());
+
+        final var aUser = User.newUser(
+                new Name("John", "Doe"),
+                new Email("john.doe@test.com"),
+                Password.of("123456Amq@"),
+                UserRole.USER
+        );
+
+        this.userRepository().save(aUser);
+
+        Assertions.assertEquals(1, countUsers());
+
+        final var aActualUser = this.userRepository().userOfEmail(aUser.getEmail().value()).orElseThrow();
+
+        Assertions.assertEquals(aUser.getId(), aActualUser.getId());
+        Assertions.assertEquals(aUser.getVersion(), aActualUser.getVersion());
+        Assertions.assertEquals(aUser.getName(), aActualUser.getName());
+        Assertions.assertEquals(aUser.getEmail().value(), aActualUser.getEmail().value());
+        Assertions.assertEquals(aUser.getPassword().value(), aActualUser.getPassword().value());
+        Assertions.assertEquals(aUser.getRole(), aActualUser.getRole());
+        Assertions.assertEquals(aUser.getCreatedAt(), aActualUser.getCreatedAt());
+        Assertions.assertEquals(aUser.getUpdatedAt(), aActualUser.getUpdatedAt());
+    }
+
+    @Test
+    void givenAnNonExistsEmail_whenCallUserOfEmail_thenReturnEmpty() {
+        Assertions.assertEquals(0, countUsers());
+
+        final var aEmail = "john.doe@gmail.ts";
+
+        final var aActualUser = this.userRepository().userOfEmail(aEmail);
+
+        Assertions.assertTrue(aActualUser.isEmpty());
+    }
+
+    @Test
+    void givenAValidId_whenCallUserOfId_thenReturnUser() {
+        Assertions.assertEquals(0, countUsers());
+
+        final var aUser = User.newUser(
+                new Name("John", "Doe"),
+                new Email("john.doe@test.com"),
+                Password.of("123456Amq@"),
+                UserRole.USER
+        );
+
+        this.userRepository().save(aUser);
+
+        Assertions.assertEquals(1, countUsers());
+
+        final var aActualUser = this.userRepository().userOfId(aUser.getId().value().toString()).orElseThrow();
+
+        Assertions.assertEquals(aUser.getId(), aActualUser.getId());
+        Assertions.assertEquals(aUser.getVersion(), aActualUser.getVersion());
+        Assertions.assertEquals(aUser.getName(), aActualUser.getName());
+        Assertions.assertEquals(aUser.getEmail().value(), aActualUser.getEmail().value());
+        Assertions.assertEquals(aUser.getPassword().value(), aActualUser.getPassword().value());
+        Assertions.assertEquals(aUser.getRole(), aActualUser.getRole());
+        Assertions.assertEquals(aUser.getCreatedAt(), aActualUser.getCreatedAt());
+        Assertions.assertEquals(aUser.getUpdatedAt(), aActualUser.getUpdatedAt());
+    }
+
+    @Test
+    void givenAnNonExistsId_whenCallUserOfId_thenReturnEmpty() {
+        Assertions.assertEquals(0, countUsers());
+
+        final var aId = "non-exists-id";
+
+        final var aActualUser = this.userRepository().userOfId(aId);
+
+        Assertions.assertTrue(aActualUser.isEmpty());
+    }
+
+    @Test
+    @Sql(statements = {
+            "INSERT INTO users (id, first_name, last_name, email, password, role, created_at, updated_at, version) " +
+                    "VALUES ('01JRP066XMA9GZZZZHAZZZZZYF', 'john', 'doe', 'john.doe@teste.com', '1234656Am@', 'INVALID_ROLE', NOW(), NOW(), 0)"
+    })
+    void givenAValidIdButInvalidRoleTypeStored_whenCallUserOfId_thenThrowsNotFoundException() {
+        Assertions.assertEquals(1, countUsers());
+
+        final var aId = "01JRP066XMA9GZZZZHAZZZZZYF";
+
+        final var aExpectedErrorMessage = "UserRole not found";
+
+        final var aException = Assertions.assertThrows(NotFoundException.class,
+                () -> this.userRepository().userOfId(aId));
+
+        Assertions.assertEquals(aExpectedErrorMessage, aException.getMessage());
     }
 }
