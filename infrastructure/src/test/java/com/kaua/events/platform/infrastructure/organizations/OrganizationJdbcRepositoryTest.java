e16a9cdc3c2909a9ec1f77624d896b27b25a8dc6
Kauã Pereira <zkauacodes@hotmail.com>
2025-04-25 12:19:20 -0300

    test(organization-repository): added the organization repository interface implementation tests

---
+++ infrastructure/src/test/java/com/kaua/events/platform/infrastructure/organizations/OrganizationJdbcRepositoryTest.java
@@ -0,0 +1,67 @@
+package com.kaua.events.platform.infrastructure.organizations;
+
+import com.kaua.events.platform.AbstractRepositoryTest;
+import com.kaua.events.platform.domain.organizations.Organization;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class OrganizationJdbcRepositoryTest extends AbstractRepositoryTest {
+
+    @Test
+    void testAssertDependencies() {
+        Assertions.assertNotNull(organizationRepository());
+    }
+
+    @Test
+    void givenAValidNewOrganization_whenCallSave_thenOrganizationIsPersisted() {
+        Assertions.assertEquals(0, countOrganizations());
+
+        final var aName = "organization-test";
+        final var aDescription = "teste";
+
+        final var aOrganization = Organization.newOrganization(
+                aName,
+                aDescription
+        );
+
+        final var aActualOrganization = this.organizationRepository().save(aOrganization);
+
+        Assertions.assertEquals(1, countOrganizations());
+        Assertions.assertEquals(aOrganization.getId(), aActualOrganization.getId());
+        Assertions.assertEquals(aOrganization.getVersion(), aActualOrganization.getVersion());
+        Assertions.assertEquals(aOrganization.getName(), aActualOrganization.getName());
+        Assertions.assertEquals(aOrganization.getDescription(), aActualOrganization.getDescription());
+        Assertions.assertEquals(aOrganization.getCreatedAt(), aActualOrganization.getCreatedAt());
+        Assertions.assertEquals(aOrganization.getUpdatedAt(), aActualOrganization.getUpdatedAt());
+    }
+
+    @Test
+    void givenAnNonExistsName_whenCallExistsByName_thenReturnFalse() {
+        Assertions.assertEquals(0, countOrganizations());
+        final var aName = "organization-test";
+
+        final var aActualResponse = this.organizationRepository().existsByName(aName);
+
+        Assertions.assertFalse(aActualResponse);
+    }
+
+    @Test
+    void givenAnExistsName_whenCallExistsByName_thenReturnTrue() {
+        Assertions.assertEquals(0, countOrganizations());
+
+        final var aName = "organization-test";
+        final var aDescription = "teste";
+
+        final var aOrganization = Organization.newOrganization(
+                aName,
+                aDescription
+        );
+        this.organizationRepository().save(aOrganization);
+
+        Assertions.assertEquals(1, countOrganizations());
+
+        final var aActualResponse = this.organizationRepository().existsByName(aName);
+
+        Assertions.assertTrue(aActualResponse);
+    }
+}
