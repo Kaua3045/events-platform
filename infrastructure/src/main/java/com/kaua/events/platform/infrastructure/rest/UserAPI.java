a03c500a1ace68154cdb0a143ad0a7cdaba2edab
Kauã Pereira <zkauacodes@hotmail.com>
2025-05-01 22:33:40 -0300

    feat(user-api): added the get me user route method

--- infrastructure/src/main/java/com/kaua/events/platform/infrastructure/rest/UserAPI.java
+++ infrastructure/src/main/java/com/kaua/events/platform/infrastructure/rest/UserAPI.java
@@ -1,14 +1,18 @@
 package com.kaua.events.platform.infrastructure.rest;
 
+import com.kaua.events.platform.infrastructure.configurations.authentication.AuthenticatedUser;
 import com.kaua.events.platform.infrastructure.idempotency.IdempotencyKey;
 import com.kaua.events.platform.infrastructure.users.req.CreateUserRequest;
 import com.kaua.events.platform.infrastructure.users.res.CreateUserResponse;
+import com.kaua.events.platform.infrastructure.users.res.GetUserByIdResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -30,4 +34,18 @@ public interface UserAPI {
             @ApiResponse(responseCode = "500", description = "Internal server error")
     })
     ResponseEntity<CreateUserResponse> createUser(@RequestBody CreateUserRequest request);
+
+    @GetMapping(
+            path = "/me",
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    @Operation(summary = "Get the authenticated user")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "User retrieved successfully"),
+            @ApiResponse(responseCode = "401", description = "Unauthorized"),
+            @ApiResponse(responseCode = "403", description = "Forbidden"),
+            @ApiResponse(responseCode = "404", description = "User identifier was not found"),
+            @ApiResponse(responseCode = "500", description = "Internal server error")
+    })
+    ResponseEntity<GetUserByIdResponse> getMe(@AuthenticationPrincipal AuthenticatedUser user);
 }
