071e32c6cd0d357c0003183553a7f899c0a2a3b5
Kauã Pereira <zkauacodes@hotmail.com>
2025-05-22 16:12:44 -0300

    feat(organization-member-repository): added the member of user id method

--- application/src/main/java/com/kaua/events/platform/application/repositories/OrganizationMemberRepository.java
+++ application/src/main/java/com/kaua/events/platform/application/repositories/OrganizationMemberRepository.java
@@ -2,7 +2,11 @@ package com.kaua.events.platform.application.repositories;
 
 import com.kaua.events.platform.domain.organizations.OrganizationMember;
 
+import java.util.Optional;
+
 public interface OrganizationMemberRepository {
+
+    Optional<OrganizationMember> memberOfUserId(String userId);
 
     OrganizationMember save(OrganizationMember organizationMember);
 }
