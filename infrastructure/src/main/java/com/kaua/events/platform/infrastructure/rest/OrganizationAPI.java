ff6d564f94066d201ebce74508ea353be3075fa4
Kauã Pereira <zkauacodes@hotmail.com>
2025-05-25 09:17:03 -0300

    feat(organization-api): added the get organization by id route method

--- infrastructure/src/main/java/com/kaua/events/platform/infrastructure/rest/OrganizationAPI.java
+++ infrastructure/src/main/java/com/kaua/events/platform/infrastructure/rest/OrganizationAPI.java
@@ -5,15 +5,14 @@ import com.kaua.events.platform.infrastructure.organizations.req.AddMemberToOrga
 import com.kaua.events.platform.infrastructure.organizations.req.CreateOrganizationRequest;
 import com.kaua.events.platform.infrastructure.organizations.res.AddMemberToOrganizationResponse;
 import com.kaua.events.platform.infrastructure.organizations.res.CreateOrganizationResponse;
+import com.kaua.events.platform.infrastructure.organizations.res.GetOrganizationByIdResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Tag(name = "Organization", description = "Organization API")
 @RequestMapping("/v1/organizations")
@@ -47,4 +46,17 @@ public interface OrganizationAPI {
             @ApiResponse(responseCode = "500", description = "Internal server error")
     })
     ResponseEntity<AddMemberToOrganizationResponse> addMemberToOrganization(@RequestBody AddMemberToOrganizationRequest request);
+
+    @GetMapping(
+            path = "/{organizationId}",
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    @Operation(summary = "Get organization by it's identifier")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Organization founded successfully"),
+            @ApiResponse(responseCode = "404", description = "Organization was not found"),
+            @ApiResponse(responseCode = "500", description = "Internal server error")
+    })
+    ResponseEntity<GetOrganizationByIdResponse> getOrganizationById(@PathVariable String organizationId);
+
 }
