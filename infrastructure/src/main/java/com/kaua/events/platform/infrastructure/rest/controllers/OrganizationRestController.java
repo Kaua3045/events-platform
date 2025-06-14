51374d3aba0151ef7cca3d997fc4d309243bd0e0
Kauã Pereira <zkauacodes@hotmail.com>
2025-06-14 11:49:36 -0300

    feat(organization-api): added the update member method implementation

--- infrastructure/src/main/java/com/kaua/events/platform/infrastructure/rest/controllers/OrganizationRestController.java
+++ infrastructure/src/main/java/com/kaua/events/platform/infrastructure/rest/controllers/OrganizationRestController.java
@@ -4,11 +4,14 @@ import com.kaua.events.platform.application.usecases.organizations.addMember.Add
 import com.kaua.events.platform.application.usecases.organizations.create.CreateOrganizationUseCase;
 import com.kaua.events.platform.application.usecases.organizations.retrieve.get.GetOrganizationByIdInput;
 import com.kaua.events.platform.application.usecases.organizations.retrieve.get.GetOrganizationByIdUseCase;
+import com.kaua.events.platform.application.usecases.organizations.update.member.UpdateMemberUseCase;
 import com.kaua.events.platform.infrastructure.organizations.req.AddMemberToOrganizationRequest;
 import com.kaua.events.platform.infrastructure.organizations.req.CreateOrganizationRequest;
+import com.kaua.events.platform.infrastructure.organizations.req.UpdateMemberRequest;
 import com.kaua.events.platform.infrastructure.organizations.res.AddMemberToOrganizationResponse;
 import com.kaua.events.platform.infrastructure.organizations.res.CreateOrganizationResponse;
 import com.kaua.events.platform.infrastructure.organizations.res.GetOrganizationByIdResponse;
+import com.kaua.events.platform.infrastructure.organizations.res.UpdateMemberResponse;
 import com.kaua.events.platform.infrastructure.rest.OrganizationAPI;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -22,15 +25,18 @@ public class OrganizationRestController implements OrganizationAPI {
     private final CreateOrganizationUseCase createOrganizationUseCase;
     private final AddMemberToOrganizationUseCase addMemberToOrganizationUseCase;
     private final GetOrganizationByIdUseCase getOrganizationByIdUseCase;
+    private final UpdateMemberUseCase updateMemberUseCase;
 
     public OrganizationRestController(
             final CreateOrganizationUseCase createOrganizationUseCase,
             final AddMemberToOrganizationUseCase addMemberToOrganizationUseCase,
-            final GetOrganizationByIdUseCase getOrganizationByIdUseCase
+            final GetOrganizationByIdUseCase getOrganizationByIdUseCase,
+            final UpdateMemberUseCase updateMemberUseCase
     ) {
         this.createOrganizationUseCase = Objects.requireNonNull(createOrganizationUseCase);
         this.addMemberToOrganizationUseCase = Objects.requireNonNull(addMemberToOrganizationUseCase);
         this.getOrganizationByIdUseCase = Objects.requireNonNull(getOrganizationByIdUseCase);
+        this.updateMemberUseCase = Objects.requireNonNull(updateMemberUseCase);
     }
 
     @Override
@@ -53,6 +59,17 @@ public class OrganizationRestController implements OrganizationAPI {
         return ResponseEntity
                 .status(HttpStatus.CREATED)
                 .body(AddMemberToOrganizationResponse.from(aOutput));
+    }
+
+    @Override
+    public ResponseEntity<UpdateMemberResponse> updateMember(final UpdateMemberRequest request) {
+        final var aInput = request.toInput();
+
+        final var aOutput = this.updateMemberUseCase.execute(aInput);
+
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(UpdateMemberResponse.from(aOutput));
     }
 
     @Override
