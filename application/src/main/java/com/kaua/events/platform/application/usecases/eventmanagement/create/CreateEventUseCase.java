9b0268718af400f6c4f723d4c617d97de9770b14
Kauã Pereira <zkauacodes@hotmail.com>
2025-06-19 11:59:13 -0300

    feat(create-event-usecase): added the create event usecase abstract class

---
+++ application/src/main/java/com/kaua/events/platform/application/usecases/eventmanagement/create/CreateEventUseCase.java
@@ -0,0 +1,7 @@
+package com.kaua.events.platform.application.usecases.eventmanagement.create;
+
+import com.kaua.events.platform.application.UseCase;
+
+public abstract class CreateEventUseCase extends
+        UseCase<CreateEventInput, CreateEventOutput> {
+}
