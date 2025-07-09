5e4a12d7a6570c4d3d1248280ee70a57307f417c
Kauã Pereira <zkauacodes@hotmail.com>
2025-07-09 14:31:20 -0300

    feat(event-repository): added the list all method with pagination

--- application/src/main/java/com/kaua/events/platform/application/repositories/EventRepository.java
+++ application/src/main/java/com/kaua/events/platform/application/repositories/EventRepository.java
@@ -1,10 +1,14 @@
 package com.kaua.events.platform.application.repositories;
 
 import com.kaua.events.platform.domain.eventmanagement.Event;
+import com.kaua.events.platform.domain.pagination.Pagination;
+import com.kaua.events.platform.domain.pagination.SearchQuery;
 
 public interface EventRepository {
 
     boolean existsByTitleAndOrganizationId(String title, String id);
+
+    Pagination<Event> listAll(SearchQuery query);
 
     Event save(Event event);
 }
