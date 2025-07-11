af1dfe640f247750de0a264b46cf58dba803af47
Kauã Pereira <zkauacodes@hotmail.com>
2025-07-11 18:38:46 -0300

    feat(create-ticket-usecase): added the create ticket usecase abstract class

---
+++ application/src/main/java/com/kaua/events/platform/application/usecases/ticket/create/CreateTicketUseCase.java
@@ -0,0 +1,6 @@
+package com.kaua.events.platform.application.usecases.ticket.create;
+
+import com.kaua.events.platform.application.UseCase;
+
+public abstract class CreateTicketUseCase extends UseCase<CreateTicketInput, CreateTicketOutput> {
+}
