24a8a3857258cd29eed36b708d45631f5a43a0f7
Kauã Pereira <zkauacodes@hotmail.com>
2025-07-11 10:37:11 -0300

    feat(soft-delete-event-usecase): added the soft delete event usecase abstract class

---
+++ application/src/main/java/com/kaua/events/platform/application/usecases/eventmanagement/delete/SoftDeleteEventUseCase.java
@@ -0,0 +1,6 @@
+package com.kaua.events.platform.application.usecases.eventmanagement.delete;
+
+import com.kaua.events.platform.application.UnitUseCase;
+
+public abstract class SoftDeleteEventUseCase extends UnitUseCase<SoftDeleteEventInput> {
+}
