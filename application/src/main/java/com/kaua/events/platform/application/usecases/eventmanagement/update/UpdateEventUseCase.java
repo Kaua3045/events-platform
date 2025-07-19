09cdcae81b9de4eb9221cc74103cc10fa04aab39
Kauã Pereira <zkauacodes@hotmail.com>
2025-07-19 12:13:07 -0300

    feat(update-event-usecase): added the update event usecase abstract class

---
+++ application/src/main/java/com/kaua/events/platform/application/usecases/eventmanagement/update/UpdateEventUseCase.java
@@ -0,0 +1,6 @@
+package com.kaua.events.platform.application.usecases.eventmanagement.update;
+
+import com.kaua.events.platform.application.UseCase;
+
+public abstract class UpdateEventUseCase extends UseCase<UpdateEventInput, UpdateEventOutput> {
+}
