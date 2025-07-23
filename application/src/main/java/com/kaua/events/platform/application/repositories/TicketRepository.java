b6a95ba490c55595f0d10017ba202af6af680b14
Kauã Pereira <zkauacodes@hotmail.com>
2025-07-23 14:29:30 -0300

    feat(ticket-repository): added the list all method implementation

--- application/src/main/java/com/kaua/events/platform/application/repositories/TicketRepository.java
+++ application/src/main/java/com/kaua/events/platform/application/repositories/TicketRepository.java
@@ -1,8 +1,12 @@
 package com.kaua.events.platform.application.repositories;
 
+import com.kaua.events.platform.domain.pagination.Pagination;
+import com.kaua.events.platform.domain.pagination.SearchQuery;
 import com.kaua.events.platform.domain.ticket.Ticket;
 
 public interface TicketRepository {
+
+    Pagination<Ticket> listAll(SearchQuery query);
 
     Ticket save(Ticket ticket);
 }
