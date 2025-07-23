8d7b707517a432d161078a7324caa83ea01b9b7c
Kauã Pereira <zkauacodes@hotmail.com>
2025-07-23 14:32:00 -0300

    feat(ticket-api): added the list tickets route method

--- infrastructure/src/main/java/com/kaua/events/platform/infrastructure/rest/TicketAPI.java
+++ infrastructure/src/main/java/com/kaua/events/platform/infrastructure/rest/TicketAPI.java
@@ -1,9 +1,11 @@
 package com.kaua.events.platform.infrastructure.rest;
 
+import com.kaua.events.platform.domain.pagination.Pagination;
 import com.kaua.events.platform.infrastructure.configurations.authentication.AuthenticatedUser;
 import com.kaua.events.platform.infrastructure.idempotency.IdempotencyKey;
 import com.kaua.events.platform.infrastructure.ticket.req.CreateTicketRequest;
 import com.kaua.events.platform.infrastructure.ticket.res.CreateTicketResponse;
+import com.kaua.events.platform.infrastructure.ticket.res.ListTicketsResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
@@ -11,9 +13,9 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @Tag(name = "Ticket", description = "Ticket API")
 @RequestMapping("/v1/tickets")
@@ -34,5 +36,24 @@ public interface TicketAPI {
     ResponseEntity<CreateTicketResponse> createTicket(
             @AuthenticationPrincipal AuthenticatedUser authenticatedUser,
             @RequestBody CreateTicketRequest request
+    );
+
+    @GetMapping(
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    @Operation(summary = "Get all tickets")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Tickets successfully found"),
+            @ApiResponse(responseCode = "500", description = "Internal server error")
+    })
+    Pagination<ListTicketsResponse> listTickets(
+            @RequestParam Map<String, String> filters,
+            @RequestParam(name = "search", required = false, defaultValue = "") String search,
+            @RequestParam(name = "page", required = false, defaultValue = "0") int page,
+            @RequestParam(name = "perPage", required = false, defaultValue = "10") int perPage,
+            @RequestParam(name = "sort", required = false, defaultValue = "role") String sort,
+            @RequestParam(name = "direction", required = false, defaultValue = "asc") String direction,
+            @RequestParam(name = "startDate", required = false) String startDate,
+            @RequestParam(name = "endDate", required = false) String endDate
     );
 }
