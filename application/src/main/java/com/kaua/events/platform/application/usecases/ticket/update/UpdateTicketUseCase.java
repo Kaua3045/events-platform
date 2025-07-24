a62ee8518b0d2ca919e3ab0bc2e09f1365c26f16
Kauã Pereira <zkauacodes@hotmail.com>
2025-07-24 09:46:16 -0300

    feat(update-ticket-usecase): added the update ticket usecase abstract class

---
+++ application/src/main/java/com/kaua/events/platform/application/usecases/ticket/update/UpdateTicketUseCase.java
@@ -0,0 +1,6 @@
+package com.kaua.events.platform.application.usecases.ticket.update;
+
+import com.kaua.events.platform.application.UseCase;
+
+public abstract class UpdateTicketUseCase extends UseCase<UpdateTicketInput, UpdateTicketOutput> {
+}
