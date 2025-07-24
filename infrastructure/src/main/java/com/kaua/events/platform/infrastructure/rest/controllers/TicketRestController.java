17fe2223a43c3ff1b66381cf4d3375cd0ea4be9c
Kauã Pereira <zkauacodes@hotmail.com>
2025-07-24 10:08:47 -0300

    feat(ticket-api): added the get ticket by id route method implementation

--- infrastructure/src/main/java/com/kaua/events/platform/infrastructure/rest/controllers/TicketRestController.java
+++ infrastructure/src/main/java/com/kaua/events/platform/infrastructure/rest/controllers/TicketRestController.java
@@ -1,6 +1,8 @@
 package com.kaua.events.platform.infrastructure.rest.controllers;
 
 import com.kaua.events.platform.application.usecases.ticket.create.CreateTicketUseCase;
+import com.kaua.events.platform.application.usecases.ticket.retrieve.get.GetTicketByIdInput;
+import com.kaua.events.platform.application.usecases.ticket.retrieve.get.GetTicketByIdUseCase;
 import com.kaua.events.platform.application.usecases.ticket.retrieve.list.ListTicketsUseCase;
 import com.kaua.events.platform.application.usecases.ticket.update.UpdateTicketUseCase;
 import com.kaua.events.platform.domain.pagination.Pagination;
@@ -11,6 +13,7 @@ import com.kaua.events.platform.infrastructure.rest.TicketAPI;
 import com.kaua.events.platform.infrastructure.ticket.req.CreateTicketRequest;
 import com.kaua.events.platform.infrastructure.ticket.req.UpdateTicketRequest;
 import com.kaua.events.platform.infrastructure.ticket.res.CreateTicketResponse;
+import com.kaua.events.platform.infrastructure.ticket.res.GetTicketByIdResponse;
 import com.kaua.events.platform.infrastructure.ticket.res.ListTicketsResponse;
 import com.kaua.events.platform.infrastructure.ticket.res.UpdateTicketResponse;
 import org.springframework.http.HttpStatus;
@@ -27,15 +30,18 @@ public class TicketRestController implements TicketAPI {
     private final CreateTicketUseCase createTicketUseCase;
     private final ListTicketsUseCase listTicketsUseCase;
     private final UpdateTicketUseCase updateTicketUseCase;
+    private final GetTicketByIdUseCase getTicketByIdUseCase;
 
     public TicketRestController(
             final CreateTicketUseCase createTicketUseCase,
             final ListTicketsUseCase listTicketsUseCase,
-            final UpdateTicketUseCase updateTicketUseCase
+            final UpdateTicketUseCase updateTicketUseCase,
+            final GetTicketByIdUseCase getTicketByIdUseCase
     ) {
         this.createTicketUseCase = Objects.requireNonNull(createTicketUseCase);
         this.listTicketsUseCase = Objects.requireNonNull(listTicketsUseCase);
         this.updateTicketUseCase = Objects.requireNonNull(updateTicketUseCase);
+        this.getTicketByIdUseCase = Objects.requireNonNull(getTicketByIdUseCase);
     }
 
     @Override
@@ -84,6 +90,17 @@ public class TicketRestController implements TicketAPI {
         final var aOutput = this.listTicketsUseCase.execute(aQuery);
 
         return aOutput.map(ListTicketsResponse::from);
+    }
+
+    @Override
+    public ResponseEntity<GetTicketByIdResponse> getTicketById(final String ticketId) {
+        final var aInput = GetTicketByIdInput.with(ticketId);
+
+        final var aOutput = this.getTicketByIdUseCase.execute(aInput);
+
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(GetTicketByIdResponse.from(aOutput));
     }
 
     @Override
