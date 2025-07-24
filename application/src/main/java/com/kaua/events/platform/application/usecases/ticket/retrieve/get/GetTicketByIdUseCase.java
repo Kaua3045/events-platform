928ef9fb70f530e633944f4948abfb9063095a81
Kauã Pereira <zkauacodes@hotmail.com>
2025-07-24 10:07:19 -0300

    feat(get-ticket-by-id-usecase): added the get ticket by id usecase abstract class

---
+++ application/src/main/java/com/kaua/events/platform/application/usecases/ticket/retrieve/get/GetTicketByIdUseCase.java
@@ -0,0 +1,6 @@
+package com.kaua.events.platform.application.usecases.ticket.retrieve.get;
+
+import com.kaua.events.platform.application.UseCase;
+
+public abstract class GetTicketByIdUseCase extends UseCase<GetTicketByIdInput, GetTicketByIdOutput> {
+}
