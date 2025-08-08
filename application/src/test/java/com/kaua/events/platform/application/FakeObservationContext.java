802ea97cded4fb4e1d067fb0f3368c3bca64f404
Kauã Pereira <zkauacodes@hotmail.com>
2025-08-08 12:50:11 -0300

    test(fake-observation-context): added this to use in tests

---
+++ application/src/test/java/com/kaua/events/platform/application/FakeObservationContext.java
@@ -0,0 +1,38 @@
+package com.kaua.events.platform.application;
+
+import com.kaua.events.platform.application.wrapper.ObservationContext;
+
+import java.util.concurrent.Callable;
+
+public class FakeObservationContext implements ObservationContext {
+
+    @Override
+    public void setAttribute(String key, Object value) {
+        // no-op
+    }
+
+    @Override
+    public void recordException(Throwable t) {
+        // no-op
+    }
+
+    @Override
+    public void addEvent(String name) {
+        // no-op
+    }
+
+    @Override
+    public <T> T runInSpanInternal(String name, Callable<T> block) {
+        try {
+            return block.call();
+        } catch (Exception e) {
+            sneakyThrow(e);
+            return null;
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    private static <E extends Throwable> void sneakyThrow(Throwable ex) throws E {
+        throw (E) ex;
+    }
+}
