322d058eb9bf66424acc9577ec64e9445a198c4f
Kauã Pereira <zkauacodes@hotmail.com>
2025-08-15 22:30:08 -0300

    feat(jdbc-config): added the named parameter jdbc operations in jdbc client adapter

--- infrastructure/src/main/java/com/kaua/events/platform/infrastructure/configurations/JdbcConfig.java
+++ infrastructure/src/main/java/com/kaua/events/platform/infrastructure/configurations/JdbcConfig.java
@@ -4,13 +4,14 @@ import com.kaua.events.platform.infrastructure.jdbc.DatabaseClient;
 import com.kaua.events.platform.infrastructure.jdbc.JdbcClientAdapter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcOperations;
 import org.springframework.jdbc.core.simple.JdbcClient;
 
 @Configuration(proxyBeanMethods = false)
 public class JdbcConfig {
 
     @Bean
-    public DatabaseClient databaseClient(final JdbcClient jdbcClient) {
-        return new JdbcClientAdapter(jdbcClient);
+    public DatabaseClient databaseClient(final JdbcClient jdbcClient, final NamedParameterJdbcOperations namedParameterJdbcOperations) {
+        return new JdbcClientAdapter(jdbcClient, namedParameterJdbcOperations);
     }
 }
