98761db67df72f9bcf5ca8a2e8823fc0a4f18768
Kauã Pereira <zkauacodes@hotmail.com>
2025-08-17 00:16:49 -0300

    feat(order-api): added the list orders route method

--- infrastructure/src/main/java/com/kaua/events/platform/infrastructure/rest/OrderAPI.java
+++ infrastructure/src/main/java/com/kaua/events/platform/infrastructure/rest/OrderAPI.java
@@ -1,8 +1,10 @@
 package com.kaua.events.platform.infrastructure.rest;
 
+import com.kaua.events.platform.domain.pagination.Pagination;
 import com.kaua.events.platform.infrastructure.configurations.authentication.AuthenticatedUser;
 import com.kaua.events.platform.infrastructure.orders.req.CreateCheckoutRequest;
 import com.kaua.events.platform.infrastructure.orders.res.CreateCheckoutResponse;
+import com.kaua.events.platform.infrastructure.orders.res.ListOrdersByUserIdResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
@@ -10,9 +12,9 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @Tag(name = "Order", description = "Order API")
 @RequestMapping("/v1/orders")
@@ -32,5 +34,25 @@ public interface OrderAPI {
     ResponseEntity<CreateCheckoutResponse> createCheckout(
             @AuthenticationPrincipal AuthenticatedUser authenticatedUser,
             @RequestBody CreateCheckoutRequest request
+    );
+
+    @GetMapping(
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    @Operation(summary = "Get all orders for authenticated user")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Orders successfully found"),
+            @ApiResponse(responseCode = "500", description = "Internal server error")
+    })
+    Pagination<ListOrdersByUserIdResponse> listOrders(
+            @RequestParam Map<String, String> filters,
+            @RequestParam(name = "search", required = false, defaultValue = "") String search,
+            @RequestParam(name = "page", required = false, defaultValue = "0") int page,
+            @RequestParam(name = "perPage", required = false, defaultValue = "10") int perPage,
+            @RequestParam(name = "sort", required = false, defaultValue = "createdAt") String sort,
+            @RequestParam(name = "direction", required = false, defaultValue = "desc") String direction,
+            @RequestParam(name = "startDate", required = false) String startDate,
+            @RequestParam(name = "endDate", required = false) String endDate,
+            @AuthenticationPrincipal AuthenticatedUser user
     );
 }
