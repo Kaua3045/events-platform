b26c94efc31c3ed5b496a158759d624613183167
Kauã Pereira <zkauacodes@hotmail.com>
2025-08-17 00:01:33 -0300

    feat(order-repository): added the list all method

--- application/src/main/java/com/kaua/events/platform/application/repositories/OrderRepository.java
+++ application/src/main/java/com/kaua/events/platform/application/repositories/OrderRepository.java
@@ -1,8 +1,12 @@
 package com.kaua.events.platform.application.repositories;
 
 import com.kaua.events.platform.domain.orders.Order;
+import com.kaua.events.platform.domain.pagination.Pagination;
+import com.kaua.events.platform.domain.pagination.SearchQuery;
 
 public interface OrderRepository {
+
+    Pagination<Order> listAll(SearchQuery query);
 
     Order save(Order order);
 }
