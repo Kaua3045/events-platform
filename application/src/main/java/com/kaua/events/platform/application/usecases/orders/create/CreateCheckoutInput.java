8fdccb4c08f9f04a8b8214b919588ef5e53e0fff
Kauã Pereira <zkauacodes@hotmail.com>
2025-08-19 11:27:49 -0300

    feat(create-checkout-input): added the document type and number, in future send this data in event

--- application/src/main/java/com/kaua/events/platform/application/usecases/orders/create/CreateCheckoutInput.java
+++ application/src/main/java/com/kaua/events/platform/application/usecases/orders/create/CreateCheckoutInput.java
@@ -6,15 +6,19 @@ import java.util.List;
 
 public record CreateCheckoutInput(
         String userId,
+        String documentNumber,
+        String documentType,
         List<CreateCheckoutItemsInput> items,
         CreateCheckoutPaymentDetailsInput paymentDetails
 ) {
 
     public static CreateCheckoutInput with(
             final String userId,
+            final String documentNumber,
+            final String documentType,
             final List<CreateCheckoutItemsInput> items,
             final CreateCheckoutPaymentDetailsInput paymentDetails
     ) {
-        return new CreateCheckoutInput(userId, items, paymentDetails);
+        return new CreateCheckoutInput(userId, documentNumber, documentType, items, paymentDetails);
     }
 }
