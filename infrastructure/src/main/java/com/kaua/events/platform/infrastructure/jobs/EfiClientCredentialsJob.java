44ab4d32ed52b12cbf1d49ed97e2b98c9c801502
Kauã Pereira <zkauacodes@hotmail.com>
2025-08-20 09:45:26 -0300

    feat(efi-client-credentials-job): added the efi client credentials job to refresh oauth token

---
+++ infrastructure/src/main/java/com/kaua/events/platform/infrastructure/jobs/EfiClientCredentialsJob.java
@@ -0,0 +1,35 @@
+package com.kaua.events.platform.infrastructure.jobs;
+
+import com.kaua.events.platform.infrastructure.configurations.authentication.client.RefreshClientCredentials;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Profile;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.util.Objects;
+import java.util.concurrent.TimeUnit;
+
+@Component
+@Profile("!test-integration")
+public class EfiClientCredentialsJob {
+
+    private static final Logger log = LoggerFactory.getLogger(EfiClientCredentialsJob.class);
+
+    private final RefreshClientCredentials refreshClientCredentials;
+
+    public EfiClientCredentialsJob(final RefreshClientCredentials refreshClientCredentials) {
+        this.refreshClientCredentials = Objects.requireNonNull(refreshClientCredentials);
+    }
+
+    @Scheduled(
+            fixedRateString = "${jobs.efi.client-credentials.refresh-rate-minutes}",
+            initialDelayString = "${jobs.efi.client-credentials.refresh-initial-delay-minutes}",
+            timeUnit = TimeUnit.MINUTES
+    )
+    public void refreshClientCredentials() {
+        log.info("Refreshing efi client credentials");
+        this.refreshClientCredentials.refresh();
+        log.info("Client efi credentials refreshed");
+    }
+}
