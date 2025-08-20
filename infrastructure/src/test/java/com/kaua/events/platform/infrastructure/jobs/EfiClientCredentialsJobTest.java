b5e188b7fbe369e946e89f2296cd26831917a073
Kauã Pereira <zkauacodes@hotmail.com>
2025-08-20 09:48:03 -0300

    test(efi-client-credentials-job): added the efi client credentials job to refresh oauth token tests

---
+++ infrastructure/src/test/java/com/kaua/events/platform/infrastructure/jobs/EfiClientCredentialsJobTest.java
@@ -0,0 +1,28 @@
+package com.kaua.events.platform.infrastructure.jobs;
+
+import com.kaua.events.platform.domain.UnitTest;
+import com.kaua.events.platform.infrastructure.configurations.authentication.client.RefreshClientCredentials;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
+class EfiClientCredentialsJobTest extends UnitTest {
+
+    @Mock
+    private RefreshClientCredentials refreshClientCredentials;
+
+    @InjectMocks
+    private EfiClientCredentialsJob clientCredentialsJob;
+
+    @Test
+    void shouldRefreshClientCredentials() {
+        clientCredentialsJob.refreshClientCredentials();
+
+        verify(refreshClientCredentials).refresh();
+    }
+}
