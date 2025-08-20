b56b8f1f79207091f44069e029088839113bb318
Kauã Pereira <zkauacodes@hotmail.com>
2025-08-20 09:37:32 -0300

    feat(payment-status): added the payment status enum

---
+++ domain/src/main/java/com/kaua/events/platform/domain/payments/PaymentStatus.java
@@ -0,0 +1,20 @@
+package com.kaua.events.platform.domain.payments;
+
+import java.util.Arrays;
+import java.util.Optional;
+
+public enum PaymentStatus {
+
+    NEW,
+    PENDING,
+    PAID,
+    FAILED,
+    EXPIRED,
+    CANCELLED;
+
+    public static Optional<PaymentStatus> from(final String value) {
+        return Arrays.stream(values())
+                .filter(it -> it.name().equalsIgnoreCase(value))
+                .findFirst();
+    }
+}
