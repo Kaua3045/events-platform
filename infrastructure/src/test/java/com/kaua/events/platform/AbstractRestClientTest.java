c09f32e2db3f80c956e5c8a9b7785fe815867a74
Kauã Pereira <zkauacodes@hotmail.com>
2025-08-20 10:10:34 -0300

    test(abstract-rest-client): added the abstract rest client for tests

---
+++ infrastructure/src/test/java/com/kaua/events/platform/AbstractRestClientTest.java
@@ -0,0 +1,35 @@
+package com.kaua.events.platform;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.tomakehurst.wiremock.client.WireMock;
+import com.kaua.events.platform.infrastructure.configurations.WebServerConfig;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Tag;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
+import org.springframework.test.context.ActiveProfiles;
+
+@ActiveProfiles("test-integration")
+@AutoConfigureWireMock(port = 0)
+@SpringBootTest(classes = {WebServerConfig.class})
+@Tag("integrationTest")
+public class AbstractRestClientTest {
+
+    @Autowired
+    private ObjectMapper mapper;
+
+    @BeforeEach
+    void beforeEach() {
+        WireMock.reset();
+        WireMock.resetAllRequests();
+    }
+
+    protected String writeValueAsString(final Object obj) {
+        try {
+            return mapper.writeValueAsString(obj);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
