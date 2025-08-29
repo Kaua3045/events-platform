2c06e0ee89c850a9d6ac58eb5f69fafe0e7bc82d
Kauã Pereira <zkauacodes@hotmail.com>
2025-08-29 16:19:08 -0300

    feat(create-checkout-response): added the qrcode image url field

--- infrastructure/src/main/java/com/kaua/events/platform/infrastructure/orders/res/CreateCheckoutResponse.java
+++ infrastructure/src/main/java/com/kaua/events/platform/infrastructure/orders/res/CreateCheckoutResponse.java
@@ -6,14 +6,16 @@ import com.kaua.events.platform.application.usecases.orders.create.CreateCheckou
 public record CreateCheckoutResponse(
         @JsonProperty("order_id") String orderId,
         @JsonProperty("payment_method") String paymentMethod,
-        @JsonProperty("qr_code_url") String qrCodeUrl
+        @JsonProperty("pix") String qrCodeUrl,
+        @JsonProperty("qr_code_image_url") String qrCodeImageUrl
 ) {
 
     public static CreateCheckoutResponse from(final CreateCheckoutOutput aOutput) {
         return new CreateCheckoutResponse(
                 aOutput.getOrderId(),
                 aOutput.getPaymentMethod(),
-                aOutput.getQrCodeUrl().orElse(null)
+                aOutput.getQrCodeUrl().orElse(null),
+                aOutput.getQrCodeImageUrl().orElse(null)
         );
     }
 }
