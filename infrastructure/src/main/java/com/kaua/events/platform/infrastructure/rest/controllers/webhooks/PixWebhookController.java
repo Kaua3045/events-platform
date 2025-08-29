8b45757544461072ee29c7e3d123b6c3e17c8a06
Kauã Pereira <zkauacodes@hotmail.com>
2025-08-29 16:20:57 -0300

    feat(pix-webhook-controller): added the pix webhook controller for receive pix webhooks

--- infrastructure/src/main/java/com/kaua/events/platform/infrastructure/rest/controllers/webhooks/PixWebhookController.java
+++ infrastructure/src/main/java/com/kaua/events/platform/infrastructure/rest/controllers/webhooks/PixWebhookController.java
@@ -20,6 +20,9 @@ public class PixWebhookController {
             @RequestBody Map<String, Object> payload
     ) {
         log.info("[PIX] Webhook recebido. Headers={} Payload={}", headers, payload);
+        payload.forEach((key, value) -> System.out.println("pix payload " + key + " " + value));
+//      12:45:26.481-0300 [tomcat-handler-9] [ ] [ ] [anonymous] INFO  c.k.e.p.i.r.c.w.PixWebhookController - [PIX] Webhook recebido. Headers={host=23db661cecfe.ngrok-free.app, user-agent=API Pix Efi (homologacao), content-length=364, content-type=application/json;charset=utf-8, x-forwarded-for=34.193.116.226, x-forwarded-host=23db661cecfe.ngrok-free.app, x-forwarded-proto=https, accept-encoding=gzip} Payload={pix=[{endToEndId=E09089356202508221544APIb936fb54, txid=ca1b8194ccf14c518b5d46adbb28684e, chave=71cdf9ba-c695-4e3c-b010-abb521a3f1be, valor=9.00, horario=2025-08-22T15:44:56.000Z, infoPagador=Teste de pagamento em ambiente sandbox, gnExtras={pagador={nome=CONSULTORIA TÉCNICA EFÍ, cnpj=09089356000118, codigoBanco=09089356}}}]}
+//pix payload pix [{endToEndId=E09089356202508221544APIb936fb54, txid=ca1b8194ccf14c518b5d46adbb28684e, chave=71cdf9ba-c695-4e3c-b010-abb521a3f1be, valor=9.00, horario=2025-08-22T15:44:56.000Z, infoPagador=Teste de pagamento em ambiente sandbox, gnExtras={pagador={nome=CONSULTORIA TÉCNICA EFÍ, cnpj=09089356000118, codigoBanco=09089356}}}]
         return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 }
