5f1a1e02dd4eb252a055d019d2536a2ebc6c0231
Kauã Pereira <zkauacodes@hotmail.com>
2025-08-29 16:30:56 -0300

    feat(efi-authentication-gateway): added the retry annotation

--- infrastructure/src/main/java/com/kaua/events/platform/infrastructure/configurations/authentication/client/efi/EfiAuthenticationGateway.java
+++ infrastructure/src/main/java/com/kaua/events/platform/infrastructure/configurations/authentication/client/efi/EfiAuthenticationGateway.java
@@ -4,6 +4,9 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import com.kaua.events.platform.infrastructure.configurations.annotations.EfiClient;
 import com.kaua.events.platform.infrastructure.configurations.authentication.client.AuthenticationGateway;
 import com.kaua.events.platform.infrastructure.configurations.properties.payments.EfiPixProperties;
+import com.kaua.events.platform.infrastructure.gateways.helpers.ReactiveHttpClientUtils;
+import io.github.resilience4j.retry.annotation.Retry;
+import io.opentelemetry.api.trace.Tracer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
@@ -18,37 +21,42 @@ import java.util.Objects;
 
 @Component
 @ConditionalOnProperty(prefix = "payments.efi.pix", name = "enabled", havingValue = "true")
-public class EfiAuthenticationGateway implements AuthenticationGateway {
+public class EfiAuthenticationGateway implements AuthenticationGateway, ReactiveHttpClientUtils {
+
+    public static final String NAMESPACE_NAME = "efi-authentication";
 
     private static final Logger log = LoggerFactory.getLogger(EfiAuthenticationGateway.class);
 
     private final WebClient webClient;
     private final String tokenUri;
+    private final Tracer tracer;
 
     public EfiAuthenticationGateway(
             @EfiClient final WebClient webClient,
-            final EfiPixProperties efiPixProperties
+            final EfiPixProperties efiPixProperties,
+            final Tracer tracer
     ) {
-        this.webClient = Objects.requireNonNull(webClient);
+        this.webClient = Objects.requireNonNull(instrument(webClient));
         this.tokenUri = Objects.requireNonNull(efiPixProperties.getOauthTokenPath());
+        this.tracer = Objects.requireNonNull(tracer);
     }
 
-    // TODO    @Retry(name = NAMESPACE_NAME)
+    @Retry(name = NAMESPACE_NAME)
     @Override
     public AuthenticationResult login(final ClientCredentialsInput input) {
-        log.debug("Creating efi client credentials");
+        log.info("Creating efi client credentials");
 
         final var aCredentials = input.clientId() + ":" + input.clientSecret();
         final var aEncodedCredentials = Base64.getEncoder().encodeToString(aCredentials.getBytes());
 
-        final var aOutput = this.webClient.post()
+        final var aOutput = doPost(() -> this.webClient.post()
                 .uri(tokenUri)
                 .contentType(MediaType.APPLICATION_JSON)
                 .header(HttpHeaders.AUTHORIZATION, "Basic " + aEncodedCredentials)
                 .bodyValue(Map.of("grant_type", "client_credentials"))
                 .retrieve()
                 .bodyToMono(AuthServerAuthenticationResult.class)
-                .block();
+                .block());
 
         // TODO need validate output return null for prevent null pointer exception
 
@@ -57,6 +65,22 @@ public class EfiAuthenticationGateway implements AuthenticationGateway {
         return new AuthenticationResult(aOutput.accessToken);
     }
 
-    record AuthServerAuthenticationResult(@JsonProperty("access_token") String accessToken, @JsonProperty("scope") String scope) {
+    @Override
+    public String namespace() {
+        return NAMESPACE_NAME;
+    }
+
+    @Override
+    public Logger logger() {
+        return log;
+    }
+
+    @Override
+    public Tracer tracer() {
+        return tracer;
+    }
+
+    record AuthServerAuthenticationResult(@JsonProperty("access_token") String accessToken,
+                                          @JsonProperty("scope") String scope) {
     }
 }
