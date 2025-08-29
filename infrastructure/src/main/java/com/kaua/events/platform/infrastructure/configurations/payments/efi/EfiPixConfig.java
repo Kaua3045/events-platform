e8d1f7b9381621b3b999e124c5cf26ef2f56b6b5
Kauã Pereira <zkauacodes@hotmail.com>
2025-08-29 16:14:17 -0300

    feat(efi-pix-config): configure webclient with timeout properties, configure efi webclient properties, added the conditional on property to enable this class

--- infrastructure/src/main/java/com/kaua/events/platform/infrastructure/configurations/payments/efi/EfiPixConfig.java
+++ infrastructure/src/main/java/com/kaua/events/platform/infrastructure/configurations/payments/efi/EfiPixConfig.java
@@ -1,59 +1,85 @@
-package com.kaua.events.platform.infrastructure.configurations.efi;
+package com.kaua.events.platform.infrastructure.configurations.payments.efi;
 
 import com.kaua.events.platform.application.gateways.PaymentGateway;
 import com.kaua.events.platform.infrastructure.configurations.annotations.EfiClient;
 import com.kaua.events.platform.infrastructure.configurations.authentication.client.GetClientCredentials;
-import com.kaua.events.platform.infrastructure.configurations.properties.EfiPixProperties;
+import com.kaua.events.platform.infrastructure.configurations.properties.WebClientProperties;
+import com.kaua.events.platform.infrastructure.configurations.properties.payments.EfiPixProperties;
 import com.kaua.events.platform.infrastructure.gateways.EfiPaymentGateway;
 import com.kaua.events.platform.infrastructure.services.certificates.LocalP12Loader;
 import com.kaua.events.platform.infrastructure.services.certificates.P12Loader;
+import io.netty.channel.ChannelOption;
 import io.netty.handler.ssl.SslContext;
+import io.netty.handler.timeout.ReadTimeoutHandler;
+import io.netty.handler.timeout.WriteTimeoutHandler;
+import io.opentelemetry.api.trace.Tracer;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.netty.http.client.HttpClient;
 
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
+
 @Configuration(proxyBeanMethods = false)
 public class EfiPixConfig {
 
     @Bean
-    @ConditionalOnProperty(prefix = "efi.pix", name = "p12-source", havingValue = "local")
+    @ConditionalOnProperty(prefix = "payments.efi.pix", name = "enabled", havingValue = "true")
+    public PaymentGateway efiPaymentGateway(
+            @EfiClient final WebClient webClient,
+            GetClientCredentials getClientCredentials,
+            EfiPixProperties efiPixProperties,
+            Tracer tracer
+    ) {
+        return new EfiPaymentGateway(
+                webClient,
+                getClientCredentials,
+                efiPixProperties,
+                tracer
+        );
+    }
+
+    @Bean
+    @ConditionalOnProperty(prefix = "payments.efi.pix", name = "p12-source", havingValue = "local")
     public P12Loader localP12Loader(final EfiPixProperties properties) {
         return new LocalP12Loader(properties.getP12LocalPath());
     }
 
+    @Bean
+    @ConditionalOnProperty(prefix = "payments.efi.pix", name = "enabled", havingValue = "true")
+    @ConfigurationProperties(prefix = "web-client.payments")
+    @EfiClient
+    public WebClientProperties efiWebClientProperties() {
+        return new WebClientProperties();
+    }
+
     @EfiClient
     @Bean
-    public WebClient efiPixWebClient(final EfiPixProperties props, final P12Loader loader) {
+    @ConditionalOnProperty(prefix = "payments.efi.pix", name = "enabled", havingValue = "true")
+    public WebClient efiPixWebClient(
+            @EfiClient final WebClientProperties webClientProperties,
+            final EfiPixProperties props,
+            final P12Loader loader
+    ) {
         byte[] p12 = loader.loadP12Bytes();
         SslContext sslContext = PixSslContextFactory.buildClientSslContext(p12, props.getP12Password());
 
-        HttpClient httpClient = HttpClient.create().secure(spec -> spec.sslContext(sslContext));
-
-//        httpClient.option(ChannelOption.CONNECT_TIMEOUT_MILLIS, properties.getConnectTimeout())
-//                .responseTimeout(Duration.ofMillis(properties.getReadTimeout()))
-//                .doOnConnected(conn ->
-//                        conn.addHandlerLast(new ReadTimeoutHandler(properties.getReadTimeout()))
-//                                .addHandlerLast(new WriteTimeoutHandler(properties.getReadTimeout())));
+        HttpClient httpClient = HttpClient.create()
+                .secure(spec -> spec.sslContext(sslContext))
+                .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, webClientProperties.getConnectTimeout())
+                .responseTimeout(Duration.ofMillis(webClientProperties.getReadTimeout()))
+                .doOnConnected(conn ->
+                        conn.addHandlerLast(new ReadTimeoutHandler(webClientProperties.getReadTimeout(), TimeUnit.MILLISECONDS))
+                                .addHandlerLast(new WriteTimeoutHandler(webClientProperties.getReadTimeout(), TimeUnit.MILLISECONDS))
+                );
 
         return WebClient.builder()
                 .baseUrl(props.getBaseUrl())
                 .clientConnector(new ReactorClientHttpConnector(httpClient))
                 .build();
-    }
-
-    @Bean
-    public PaymentGateway paymentGateway(
-            @EfiClient final WebClient webClient,
-            final GetClientCredentials getClientCredentials,
-            final EfiPixProperties efiPixProperties
-    ) {
-        return new EfiPaymentGateway(
-                webClient,
-                getClientCredentials,
-                efiPixProperties
-        );
     }
 }
