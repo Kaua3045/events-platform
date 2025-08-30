5ff149cfc77acfa38269f9e04810adc1d5b4ec78
Kauã Pereira <zkauacodes@hotmail.com>
2025-08-30 19:03:56 -0300

    feat(create-checkout-credit-card-payment-details): added the create checkout credit card payment details infos

--- application/src/main/java/com/kaua/events/platform/application/usecases/orders/create/payment/CreateCheckoutCreditCardPaymentDetails.java
+++ application/src/main/java/com/kaua/events/platform/application/usecases/orders/create/payment/CreateCheckoutCreditCardPaymentDetails.java
@@ -2,7 +2,13 @@ package com.kaua.events.platform.application.usecases.orders.create.payment;
 
 import com.kaua.events.platform.domain.payments.PaymentMethod;
 
-public final class CreateCheckoutCreditCardPaymentDetails implements CreateCheckoutPaymentDetailsInput {
+public record CreateCheckoutCreditCardPaymentDetails(
+        String name,
+        String cpf,
+        String email,
+        String paymentToken,
+        int installments
+) implements CreateCheckoutPaymentDetailsInput {
 
     @Override
     public PaymentMethod method() {
