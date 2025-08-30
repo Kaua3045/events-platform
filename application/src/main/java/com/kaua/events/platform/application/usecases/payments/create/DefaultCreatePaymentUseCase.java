6b8e1fa6239c1f8577a61f6f3db8365eaa85f625
Kauã Pereira <zkauacodes@hotmail.com>
2025-08-30 19:04:58 -0300

    feat(create-payment-usecase): added the process credit card and mark as pending on receive waiting status

--- application/src/main/java/com/kaua/events/platform/application/usecases/payments/create/DefaultCreatePaymentUseCase.java
+++ application/src/main/java/com/kaua/events/platform/application/usecases/payments/create/DefaultCreatePaymentUseCase.java
@@ -71,7 +71,16 @@ public class DefaultCreatePaymentUseCase extends CreatePaymentUseCase {
                         return CreatePaymentOutput.from(aPendingPayment);
                     }
 
-                    // TODO se nao for pix, deve salvar um evento e retornar o payment ou alguma outra coisa, talvez caso seja pix criar um caso de uso a parte
+                    if (aPayment.getMethod().equals(PaymentMethod.CREDIT_CARD) && aOutput.status().equals(PaymentProcessStatus.WAITING)) {
+                        final var aPendingPayment = aPayment.markAsPending(0, null, null);
+
+                        ctx.runInSpan(
+                                "payment.update",
+                                () -> this.paymentRepository.save(aPendingPayment)
+                        );
+
+                        return CreatePaymentOutput.from(aPendingPayment);
+                    }
 
                     return null;
                 }
