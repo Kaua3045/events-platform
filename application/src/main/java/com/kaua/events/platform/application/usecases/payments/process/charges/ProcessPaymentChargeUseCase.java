907283ea83e5713b28374af53f629a16f1c523c6
Kauã Pereira <zkauacodes@hotmail.com>
2025-08-31 14:44:16 -0300

    feat(process-payment-charge-usecase): added the process payment charge usecase abstract class

---
+++ application/src/main/java/com/kaua/events/platform/application/usecases/payments/process/charges/ProcessPaymentChargeUseCase.java
@@ -0,0 +1,6 @@
+package com.kaua.events.platform.application.usecases.payments.process.charges;
+
+import com.kaua.events.platform.application.UnitUseCase;
+
+public abstract class ProcessPaymentChargeUseCase extends UnitUseCase<ProcessPaymentChargeInput> {
+}
