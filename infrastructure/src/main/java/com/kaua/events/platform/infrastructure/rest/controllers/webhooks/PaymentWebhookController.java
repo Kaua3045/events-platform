37f15b90373c6c43a7f9fc702bf09eb4cf0fe5f0
Kauã Pereira <zkauacodes@hotmail.com>
2025-08-31 14:45:54 -0300

    feat(payment-webhook-controller): added the process payment charge usecase in card notification route

--- infrastructure/src/main/java/com/kaua/events/platform/infrastructure/rest/controllers/webhooks/PaymentWebhookController.java
+++ infrastructure/src/main/java/com/kaua/events/platform/infrastructure/rest/controllers/webhooks/PaymentWebhookController.java
@@ -1,18 +1,37 @@
 package com.kaua.events.platform.infrastructure.rest.controllers.webhooks;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.kaua.events.platform.application.usecases.payments.process.charges.ProcessPaymentChargeInput;
+import com.kaua.events.platform.application.usecases.payments.process.charges.ProcessPaymentChargeUseCase;
+import com.kaua.events.platform.infrastructure.configurations.annotations.EfiChargesClient;
+import com.kaua.events.platform.infrastructure.configurations.authentication.client.GetClientCredentials;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.reactive.function.client.WebClient;
 
+import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 
 @RestController
 @RequestMapping("/webhooks")
 public class PaymentWebhookController {
 
     private static final Logger log = LoggerFactory.getLogger(PaymentWebhookController.class);
+
+    private final ProcessPaymentChargeUseCase processPaymentChargeUseCase;
+
+    public PaymentWebhookController(
+            final ProcessPaymentChargeUseCase processPaymentChargeUseCase
+    ) {
+        this.processPaymentChargeUseCase = Objects.requireNonNull(processPaymentChargeUseCase);
+    }
 
     @PostMapping("/pix")
     public ResponseEntity<Void> receivePixWebhook(
@@ -26,13 +45,16 @@ public class PaymentWebhookController {
         return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 
-    @PostMapping("/card/notification")
+    @PostMapping(value = "/card/notification", consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
     public ResponseEntity<Void> receiveCardWebhook(
             @RequestHeader Map<String, String> headers,
-            @RequestBody Map<String, Object> payload
+            @RequestParam MultiValueMap<String, String> formParams
     ) {
-        log.info("[CreditCard] Webhook recebido. Headers={} Payload={}", headers, payload);
-        payload.forEach((key, value) -> System.out.println("credit card payload " + key + " " + value));
-        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+        log.info("[CreditCard] Webhook recebido. Headers={} Payload={}", headers, formParams);
+        this.processPaymentChargeUseCase.execute(ProcessPaymentChargeInput.with(
+                formParams.get("notification").getFirst()
+        ));
+        // TODO add log
+        return ResponseEntity.status(HttpStatus.OK).build();
     }
 }
