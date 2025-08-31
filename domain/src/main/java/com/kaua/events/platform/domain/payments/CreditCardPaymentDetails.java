3dbce88ae99e29f9e56ae201353f151815ab267c
Kauã Pereira <zkauacodes@hotmail.com>
2025-08-31 11:14:22 -0300

    feat(credit-card-payment-details): added the phone number

--- domain/src/main/java/com/kaua/events/platform/domain/payments/CreditCardPaymentDetails.java
+++ domain/src/main/java/com/kaua/events/platform/domain/payments/CreditCardPaymentDetails.java
@@ -2,7 +2,7 @@ package com.kaua.events.platform.domain.payments;
 
 import java.math.BigDecimal;
 
-public record CreditCardPaymentDetails(BigDecimal amount, String name, String cpf, String email,
+public record CreditCardPaymentDetails(BigDecimal amount, String name, String cpf, String phoneNumber, String email,
                                        String paymentToken, int installments) implements PaymentDetails {
 
     @Override
