c2a7a4af0b93df20ce18e84ecf3edf1f888dfbaa
Kauã Pereira <zkauacodes@hotmail.com>
2025-09-03 20:54:31 -0300

    refactor(create-checkout-response): change qrcode return to use payment details

--- infrastructure/src/main/java/com/kaua/events/platform/infrastructure/orders/res/CreateCheckoutResponse.java
+++ infrastructure/src/main/java/com/kaua/events/platform/infrastructure/orders/res/CreateCheckoutResponse.java
@@ -2,20 +2,19 @@ package com.kaua.events.platform.infrastructure.orders.res;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.kaua.events.platform.application.usecases.orders.create.CreateCheckoutOutput;
+import com.kaua.events.platform.domain.payments.PaymentDetails;
 
 public record CreateCheckoutResponse(
         @JsonProperty("order_id") String orderId,
         @JsonProperty("payment_method") String paymentMethod,
-        @JsonProperty("pix") String qrCodeUrl,
-        @JsonProperty("qr_code_image_url") String qrCodeImageUrl
+        @JsonProperty("payment_details") PaymentDetails paymentDetails
 ) {
 
     public static CreateCheckoutResponse from(final CreateCheckoutOutput aOutput) {
         return new CreateCheckoutResponse(
                 aOutput.getOrderId(),
                 aOutput.getPaymentMethod(),
-                aOutput.getQrCodeUrl().orElse(null),
-                aOutput.getQrCodeImageUrl().orElse(null)
+                aOutput.getPaymentDetails()
         );
     }
 }
