1d33feb889fcad42dcb9a7cf7e734a06f0504c61
Kauã Pereira <zkauacodes@hotmail.com>
2025-09-03 10:49:33 -0300

    feat(credit-card-payment-details): added the todo

--- domain/src/main/java/com/kaua/events/platform/domain/payments/CreditCardPaymentDetails.java
+++ domain/src/main/java/com/kaua/events/platform/domain/payments/CreditCardPaymentDetails.java
@@ -2,6 +2,13 @@ package com.kaua.events.platform.domain.payments;
 
 import java.math.BigDecimal;
 
+// TODO em vez de todas as infos do user, deviamos salvar so o id e brand, last4, expMont e expYear
+// e referenciar payment details no payment, mover tudo de pix do pix details
+// e salvar tudo isso no payment_details table, mas pra isso acontecer
+// precisamos primeiro adicionar cpf e phoneNumber no user
+// depois refatorar tudo de checkout pra receber o authenticatedUserId
+// dai passar pro evento, refatorar o payment pra receber os details
+// depois salvar no db
 public record CreditCardPaymentDetails(BigDecimal amount, String name, String cpf, String phoneNumber, String email,
                                        String paymentToken, int installments) implements PaymentDetails {
 
