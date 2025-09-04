2d1323011b2842a93f21ed06ea999a816b62c56c
Kauã Pereira <zkauacodes@hotmail.com>
2025-09-04 20:46:25 -0300

    feat(create-users-migration): added the document columns

--- infrastructure/src/main/resources/db/migration/V1__create-users-table.sql
+++ infrastructure/src/main/resources/db/migration/V1__create-users-table.sql
@@ -5,6 +5,8 @@ CREATE TABLE users (
     email VARCHAR(255) UNIQUE NOT NULL,
     password VARCHAR(255) NOT NULL,
     role VARCHAR(50) NOT NULL,
+    document_number VARCHAR(18) NULL,
+    document_type VARCHAR(5) NULL,
     created_at TIMESTAMP WITH TIME ZONE NOT NULL,
     updated_at TIMESTAMP WITH TIME ZONE NOT NULL,
     version BIGINT NOT NULL
