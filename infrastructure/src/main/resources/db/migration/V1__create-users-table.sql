53ec1b42309421cd4179b961597693f270e49861
Kauã Pereira <zkauacodes@hotmail.com>
2025-09-05 19:42:55 -0300

    feat(create-users-migration): added the phone_e164 column fix not null

--- infrastructure/src/main/resources/db/migration/V1__create-users-table.sql
+++ infrastructure/src/main/resources/db/migration/V1__create-users-table.sql
@@ -7,7 +7,7 @@ CREATE TABLE users (
     role VARCHAR(50) NOT NULL,
     document_number VARCHAR(18) NULL,
     document_type VARCHAR(5) NULL,
-    phone_e164 VARCHAR(20) NOT NULL UNIQUE,
+    phone_e164 VARCHAR(20) NULL UNIQUE,
     created_at TIMESTAMP WITH TIME ZONE NOT NULL,
     updated_at TIMESTAMP WITH TIME ZONE NOT NULL,
     version BIGINT NOT NULL
