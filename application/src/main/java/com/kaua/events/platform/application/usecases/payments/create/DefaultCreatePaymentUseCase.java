0b430fcab30dae134fcf8701f0c5db64f9993231
Kauã Pereira <zkauacodes@hotmail.com>
2025-09-07 13:31:24 -0300

    feat(create-payment-usecase): added the create payment details request and retrieve user on payment details contains userId

--- application/src/main/java/com/kaua/events/platform/application/usecases/payments/create/DefaultCreatePaymentUseCase.java
+++ application/src/main/java/com/kaua/events/platform/application/usecases/payments/create/DefaultCreatePaymentUseCase.java
@@ -4,12 +4,18 @@ import com.kaua.events.platform.application.exceptions.UseCaseInputCannotBeNullE
 import com.kaua.events.platform.application.gateways.PaymentGateway;
 import com.kaua.events.platform.application.gateways.PaymentGateway.PaymentProcessRequest;
 import com.kaua.events.platform.application.gateways.PaymentGateway.PaymentProcessStatus;
+import com.kaua.events.platform.application.gateways.payment.PaymentCreditCardPaymentDetailsRequest;
+import com.kaua.events.platform.application.gateways.payment.PaymentDetailsRequest;
+import com.kaua.events.platform.application.gateways.payment.PaymentPixPaymentDetailsRequest;
 import com.kaua.events.platform.application.repositories.PaymentRepository;
+import com.kaua.events.platform.application.repositories.UserRepository;
+import com.kaua.events.platform.application.wrapper.ObservationContext;
 import com.kaua.events.platform.application.wrapper.TracerWrapper;
+import com.kaua.events.platform.domain.exceptions.NotFoundException;
 import com.kaua.events.platform.domain.orders.OrderID;
-import com.kaua.events.platform.domain.payments.Payment;
-import com.kaua.events.platform.domain.payments.PaymentMethod;
-import com.kaua.events.platform.domain.payments.PixPaymentDetails;
+import com.kaua.events.platform.domain.payments.*;
+import com.kaua.events.platform.domain.person.Document;
+import com.kaua.events.platform.domain.users.User;
 import com.kaua.events.platform.domain.utils.ULID;
 
 import java.util.Objects;
@@ -18,15 +24,18 @@ public class DefaultCreatePaymentUseCase extends CreatePaymentUseCase {
 
     private final PaymentRepository paymentRepository;
     private final PaymentGateway paymentGateway;
+    private final UserRepository userRepository;
     private final TracerWrapper tracerWrapper;
 
     public DefaultCreatePaymentUseCase(
             final PaymentRepository paymentRepository,
             final PaymentGateway paymentGateway,
+            final UserRepository userRepository,
             final TracerWrapper tracerWrapper
     ) {
         this.paymentRepository = Objects.requireNonNull(paymentRepository);
         this.paymentGateway = Objects.requireNonNull(paymentGateway);
+        this.userRepository = Objects.requireNonNull(userRepository);
         this.tracerWrapper = Objects.requireNonNull(tracerWrapper);
     }
 
@@ -36,6 +45,10 @@ public class DefaultCreatePaymentUseCase extends CreatePaymentUseCase {
                 "createPaymentUseCase",
                 ctx -> {
                     if (input == null) throw new UseCaseInputCannotBeNullException(CreatePaymentUseCase.class);
+
+                    final var paymentDetails = input.paymentDetails();
+
+                    final var finalPaymentDetails = getFinalPaymentDetailsRequest(ctx, paymentDetails);
 
                     final var aPayment = Payment.newPayment(
                             new OrderID(ULID.fromString(input.orderId())),
@@ -54,7 +67,7 @@ public class DefaultCreatePaymentUseCase extends CreatePaymentUseCase {
                             () -> this.paymentGateway.process(new PaymentProcessRequest(
                                     aPayment.getTransactionId(),
                                     aPayment.getOrderId().value().toString(),
-                                    input.paymentDetails()
+                                    finalPaymentDetails
                             ))
                     );
 
@@ -90,5 +103,28 @@ public class DefaultCreatePaymentUseCase extends CreatePaymentUseCase {
                     throw new RuntimeException("Error on create payment " + aOutput + " " + aPayment);
                 }
         );
+    }
+
+    private PaymentDetailsRequest getFinalPaymentDetailsRequest(ObservationContext ctx, PaymentDetails paymentDetails) {
+        return switch (paymentDetails) {
+            case CreditCardPaymentDetails creditCardDetails -> ctx.runInSpan(
+                    "user.retrieve",
+                    () -> this.userRepository.userOfId(creditCardDetails.userId())
+                            .map(user -> new PaymentCreditCardPaymentDetailsRequest(
+                                    creditCardDetails.amount(),
+                                    user.getName().fullName(),
+                                    user.getDocument().map(Document::value).orElse(null),
+                                    user.getDocument().map(Document::type).orElse(null),
+                                    user.getEmail().value(),
+                                    user.getPhoneNumber().orElse(null),
+                                    creditCardDetails.paymentToken(),
+                                    creditCardDetails.installments()
+                            ))
+                            .orElseThrow(NotFoundException.with(User.class, creditCardDetails.userId()))
+            );
+            case PixPaymentDetails pixDetails -> new PaymentPixPaymentDetailsRequest(
+                    pixDetails.amount()
+            );
+        };
     }
 }
