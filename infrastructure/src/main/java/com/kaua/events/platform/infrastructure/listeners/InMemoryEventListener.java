12a286fef60b64f1dd9e568c3cb123496a79be36
Kauã Pereira <zkauacodes@hotmail.com>
2025-09-11 18:06:20 -0300

    feat(in-memory-event-listener): added the payment status changed handle changed event and call update order status usecase

--- infrastructure/src/main/java/com/kaua/events/platform/infrastructure/listeners/InMemoryEventListener.java
+++ infrastructure/src/main/java/com/kaua/events/platform/infrastructure/listeners/InMemoryEventListener.java
@@ -1,12 +1,13 @@
 package com.kaua.events.platform.infrastructure.listeners;
 
+import com.kaua.events.platform.application.usecases.orders.update.status.UpdateOrderStatusInput;
+import com.kaua.events.platform.application.usecases.orders.update.status.UpdateOrderStatusUseCase;
 import com.kaua.events.platform.application.usecases.payments.create.CreatePaymentInput;
 import com.kaua.events.platform.application.usecases.payments.create.CreatePaymentUseCase;
 import com.kaua.events.platform.domain.orders.events.OrderCreatedEvent;
 import com.kaua.events.platform.domain.payments.events.PaymentCreatedEvent;
 import com.kaua.events.platform.domain.payments.events.PaymentStatusChangedEvent;
 import com.kaua.events.platform.infrastructure.configurations.json.Json;
-import com.kaua.events.platform.infrastructure.outbox.OutboxJdbcRepository;
 import com.kaua.events.platform.infrastructure.outbox.OutboxJdbcRepository.OutboxMessage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -23,25 +24,39 @@ public class InMemoryEventListener {
     private static final Logger log = LoggerFactory.getLogger(InMemoryEventListener.class);
 
     private final CreatePaymentUseCase createPaymentUseCase;
+    private final UpdateOrderStatusUseCase updateOrderStatusUseCase;
 
-    public InMemoryEventListener(final CreatePaymentUseCase createPaymentUseCase) {
+    public InMemoryEventListener(
+            final CreatePaymentUseCase createPaymentUseCase,
+            final UpdateOrderStatusUseCase updateOrderStatusUseCase
+    ) {
         this.createPaymentUseCase = Objects.requireNonNull(createPaymentUseCase);
+        this.updateOrderStatusUseCase = Objects.requireNonNull(updateOrderStatusUseCase);
     }
 
     @EventListener
     public void handleEvents(OutboxMessage aOutboxMessage) {
         log.info("OutboxMessage received: {}", aOutboxMessage);
         switch (aOutboxMessage.eventType()) {
-            case "OrderCreated" -> this.handleOrderCreatedEvent(Json.readValue(aOutboxMessage.payload(), OrderCreatedEvent.class));
-            case "PaymentCreated" -> this.handlePaymentCreatedEvent(Json.readValue(aOutboxMessage.payload(), PaymentCreatedEvent.class));
-            case "PaymentStatusChanged" -> this.handlePaymentStatusChanged(Json.readValue(aOutboxMessage.payload(), PaymentStatusChangedEvent.class));
+            case "OrderCreated" ->
+                    this.handleOrderCreatedEvent(Json.readValue(aOutboxMessage.payload(), OrderCreatedEvent.class));
+            case "PaymentCreated" ->
+                    this.handlePaymentCreatedEvent(Json.readValue(aOutboxMessage.payload(), PaymentCreatedEvent.class));
+            case "PaymentStatusChanged" ->
+                    this.handlePaymentStatusChanged(Json.readValue(aOutboxMessage.payload(), PaymentStatusChangedEvent.class));
             default -> throw new IllegalArgumentException("Event type not recognized: " + aOutboxMessage.eventType());
         }
     }
 
     private void handlePaymentStatusChanged(final PaymentStatusChangedEvent paymentStatusChangedEvent) {
         log.info("Payment status changed Event received: {}", paymentStatusChangedEvent);
-        // TODO notify order
+        final var aPaymentStatusChangedProcessed = this.updateOrderStatusUseCase.execute(
+                UpdateOrderStatusInput.with(
+                        paymentStatusChangedEvent.orderId(),
+                        paymentStatusChangedEvent.status(),
+                        paymentStatusChangedEvent.aggregateId())
+        );
+        log.info("Order status changed by payment status changed event {}", aPaymentStatusChangedProcessed);
     }
 
     private void handlePaymentCreatedEvent(final PaymentCreatedEvent paymentCreatedEvent) {
